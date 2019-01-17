e3875412b60848e253203e9d6f1c19f431f90211
grzegorzu <grzegorz.urbanski@hatomi.pl>
2019-01-17 17:32:29 +0100

    initialize springboot corrected

---
+++ notebook-sb-app/src/main/java/grzegorzurbanski/notebook/notebooksbapp/NotebookSbAppApplication.java
@@ -0,0 +1,14 @@
+package grzegorzurbanski.notebook.notebooksbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookSbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookSbAppApplication.class, args);
+	}
+
+}
+
---
+++ notebook-sb-app/src/test/java/grzegorzurbanski/notebook/notebooksbapp/NotebookSbAppApplicationTests.java
@@ -0,0 +1,17 @@
+package grzegorzurbanski.notebook.notebooksbapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotebookSbAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
