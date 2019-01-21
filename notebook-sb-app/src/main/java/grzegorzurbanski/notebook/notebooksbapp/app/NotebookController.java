bde61dec7ff2b7190de900f2300df0d6ee64167f
grzegorzu <grzegorz.urbanski@hatomi.pl>
2019-01-21 13:49:44 +0100

    get all notebooks from backend to frondend

--- notebook-sb-app/src/main/java/grzegorzurbanski/notebook/notebooksbapp/app/NotebookController.java
+++ notebook-sb-app/src/main/java/grzegorzurbanski/notebook/notebooksbapp/app/NotebookController.java
@@ -1,35 +1,28 @@
 package grzegorzurbanski.notebook.notebooksbapp.app;
 
-
 import grzegorzurbanski.notebook.notebooksbapp.db.NotebookRepository;
 import grzegorzurbanski.notebook.notebooksbapp.model.Notebook;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/note")
+@RequestMapping("/api/notebooks")
 @CrossOrigin
-public class NoteController {
+public class NotebookController {
 
     private NotebookRepository notebookRepository;
 
-
-    public NoteController(NotebookRepository notebookRepository){
+    public NotebookController(NotebookRepository notebookRepository){
         this.notebookRepository = notebookRepository;
-
     }
 
     @GetMapping("/all")
     public List<Notebook> notebooks(){
         return  this.notebookRepository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public Optional<Notebook> getNotebookById(@PathVariable String id){
-        return this.notebookRepository.findById(UUID.fromString(id));
     }
 
 }
--- notebook-sb-app/src/main/java/grzegorzurbanski/notebook/notebooksbapp/app/viewmodel/NotebookViewModel.java
+++ notebook-sb-app/src/main/java/grzegorzurbanski/notebook/notebooksbapp/app/viewmodel/NotebookViewModel.java
@@ -1,5 +1,37 @@
 package grzegorzurbanski.notebook.notebooksbapp.app.viewmodel;
 
+import javax.validation.constraints.NotNull;
+
 public class NotebookViewModel {
 
+private String id;
+
+private String name;
+
+private int nbNotes;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    @NotNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNbNotes() {
+        return nbNotes;
+    }
+
+    public void setNbNotes(int nbNotes) {
+        this.nbNotes = nbNotes;
+    }
 }
